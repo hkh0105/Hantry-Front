c36e8833efb7a7333ac35782b964ff5e9985fbc3
hkh9601 <hkh9601@gmail.com>
2022-07-14 19:22:16 +0900

    modal reducer설정

--- src/store/index.js
+++ src/store/index.js
@@ -1,11 +1,13 @@
 import { configureStore } from "@reduxjs/toolkit";
 import userReducer from "./userSlice";
 import projectReducer from "./projectSlice";
+import modalSliceReducer from "./modalSlice";
 
 const store = configureStore({
   reducer: {
     user: userReducer,
     project: projectReducer,
+    modal: modalSliceReducer,
   },
 });
 
