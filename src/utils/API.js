64944a1cb3d88e0bd902039b3da544c363ee2e78
hkh9601 <hkh9601@gmail.com>
2022-07-14 23:58:49 +0900

    prod url option add

--- src/utils/API.js
+++ src/utils/API.js
@@ -1,6 +1,10 @@
 import axios from "axios";
 import { getCookie } from "cookies-next";
 
+const baseURL =
+  process.env.REACT_APP_ENV === DEV
+    ? process.env.REACT_APP_SERVER_URL
+    : process.env.REACT_APP_PROD_URL;
 const API = axios.create({
   baseURL: process.env.REACT_APP_SERVER_URL,
 });
