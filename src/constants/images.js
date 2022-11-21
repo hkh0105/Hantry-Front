ca0eedb461f8ef70976bd2cf0c0dd9c3740d351a
hkh0105 <hkh9601@naver.com>
2022-11-22 00:46:38 +0900

    Constants Webservice, images 선언

--- src/constants/images.js
+++ src/constants/images.js
@@ -1,3 +1,4 @@
 export const IMAGES = {
   logo: process.env.PUBLIC_URL + "/hantry2.jpg",
+  noError: process.env.PUBLIC_URL + "/NoError.png",
 };
---
+++ src/constants/webservice.js
@@ -0,0 +1,6 @@
+const baseURL = process.env.REACT_APP_SERVER_URL;
+
+export const WEB_SERVICE = {
+  getProjectListUrl: "/users/project",
+  getProjectErrorUrl: "/users/project/{dsn}/error",
+};
