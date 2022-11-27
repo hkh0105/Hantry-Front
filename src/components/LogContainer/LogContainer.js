c3e392a1dba1363a333dfc3da6efcadbcb2ef0a0
hkh0105 <hkh9601@naver.com>
2022-11-28 01:53:39 +0900

    LogContainer 생성

--- src/components/LogContainer/LogContainer.js
+++ src/components/LogContainer/LogContainer.js
@@ -1,5 +1,26 @@
 import "./LogContainer.scss";
 
-export default function LogContainer({ children }) {
-  return <div className="log-box">{children}</div>;
+import ErrorLog from "../ErrorLog/ErrorLog";
+import PagenatedButton from "../PaginateButton/PaginateButton";
+import WhiteWrapper from "../WhiteWrapper/WhiteWrapper";
+
+import useUserError from "../../hooks/useUserError";
+
+export default function LogContainer({ dsn }) {
+  const { errors, nextPaginationHandler, prevPaginationHandler } =
+    useUserError(dsn);
+
+  return (
+    <WhiteWrapper title={"Error Log"}>
+      <div className="log-box">
+        {errors.map((error, index) => (
+          <ErrorLog key={index} error={error} />
+        ))}
+      </div>
+      <PagenatedButton
+        prevPaginationHandler={prevPaginationHandler}
+        nextPaginationHandler={nextPaginationHandler}
+      ></PagenatedButton>
+    </WhiteWrapper>
+  );
 }
