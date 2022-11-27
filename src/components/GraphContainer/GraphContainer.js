ed162b5885e68f070834904352f120bbf79fb359
hkh0105 <hkh9601@naver.com>
2022-11-27 21:23:43 +0900

    GraphContainer Component 생성

---
+++ src/components/GraphContainer/GraphContainer.js
@@ -0,0 +1,38 @@
+import BarGraph from "../BarGraph/BarGraph";
+import LineGraph from "../LineGraph/LineGraph";
+import Dropdown from "../Dropdown/Dropdown";
+import WhiteWrapper from "../WhiteWrapper/WhiteWrapper";
+
+import { GrpahTimeList } from "../../constants";
+import Loadable from "../Loadable/Loadable";
+import useUserError from "../../hooks/useUserError";
+import useGraphFilter from "../../hooks/useGraphFilter";
+
+export default function GraphContainer({ dsn }) {
+  const { allErrors } = useUserError(dsn);
+  const { graphData, timeFilterButtonHandler } = useGraphFilter(allErrors);
+
+  return (
+    <Loadable>
+      <WhiteWrapper title={"Graph"}>
+        <Dropdown
+          optionList={GrpahTimeList}
+          defaultValue={"All"}
+          onChange={timeFilterButtonHandler}
+        ></Dropdown>
+        <div className="chart-container">
+          <BarGraph
+            inputs={graphData}
+            keys={["count"]}
+            bottom="Type"
+            indexBy="type"
+            left="Count"
+          />
+        </div>
+        <div className="chart-container">
+          <LineGraph errors={allErrors} />
+        </div>
+      </WhiteWrapper>
+    </Loadable>
+  );
+}
