dd972cdd09e520442015a086c72e33a205cb4ffd
hkh0105 <hkh9601@naver.com>
2022-11-30 23:58:09 +0900

    Image Constant 생성

--- src/constants/images.js
+++ src/constants/images.js
@@ -1,4 +1,9 @@
 export const IMAGES = {
   logo: process.env.PUBLIC_URL + "/hantry2.jpg",
   noError: process.env.PUBLIC_URL + "/NoError.png",
+  React: process.env.PUBLIC_URL + "/React.png",
+  Vue: process.env.PUBLIC_URL + "/Vue.png",
+  JavaScript: process.env.PUBLIC_URL + "/JavaScript.png",
+  Node: process.env.PUBLIC_URL + "/Node.png",
+  Angular: process.env.PUBLIC_URL + "/Angular.png",
 };
