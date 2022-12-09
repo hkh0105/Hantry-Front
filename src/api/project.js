e7402d37367ea96ab03661dd11e02185b532d357
hkh0105 <hkh9601@naver.com>
2022-12-10 01:38:01 +0900

    createNewProjectApi api 폴더로 이동

--- src/api/project.js
+++ src/api/project.js
@@ -4,3 +4,5 @@ import { WEB_SERVICE } from "../constants/webservice";
 export const getProjectListApi = () => API.get(WEB_SERVICE.getProjectListUrl);
 export const getProjectDetailsApi = async dsn =>
   API.get(WEB_SERVICE.getProjectDetailsUrl + dsn);
+export const createNewProjectApi = project =>
+  API.post("/users/project", { project });
