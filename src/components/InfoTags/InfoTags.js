721068ed681c714fa0aede8d6a03c38711d693c4
hkh0105 <hkh9601@naver.com>
2022-12-22 21:56:46 +0900

    TagContainer 컴포넌트 InfoTags 이름변경 및 받는 Prop변경

--- src/components/InfoTags/InfoTags.js
+++ src/components/InfoTags/InfoTags.js
@@ -1,14 +1,14 @@
-import "./TagContainer.scss";
+import "./InfoTags.scss";
 import { DiReact, DiChrome, DiApple, DiWindows } from "react-icons/di";
 import { FaInternetExplorer } from "react-icons/fa";
 
-export default function TagContainer({ error }) {
+export default function InfoTags({ browser, os, engine }) {
   return (
     <div className="error-detail-event-env">
       <p>TAGS</p>
       <div>
         <span>
-          {error.user && error.user.browser == "Chrome" ? (
+          {browser == "Chrome" ? (
             <DiChrome
               style={{
                 width: "60px",
@@ -26,12 +26,12 @@ export default function TagContainer({ error }) {
             />
           )}
           <div className="error-detail-version-container">
-            {error.user && error.user.browser}
+            {browser}
             <div className="error-detail-version">Version: 65.0.3325</div>
           </div>
         </span>
         <span>
-          {error.user && error.user.os == "Mac OS" ? (
+          {os == "Mac OS" ? (
             <DiApple
               style={{
                 width: "60px",
@@ -50,7 +50,7 @@ export default function TagContainer({ error }) {
           )}
 
           <div className="error-detail-version-container">
-            {error.user && error.user.os}
+            {os}
             <div className="error-detail-version">Version: 65.0.3325</div>
           </div>
         </span>
@@ -59,7 +59,7 @@ export default function TagContainer({ error }) {
             style={{ width: "60px", height: "60px", color: "#f37464" }}
           />
           <div className="error-detail-version-container">
-            {error.user && error.user.engine}
+            {engine}
             <div className="error-detail-version">Version: Null</div>
           </div>
         </span>
