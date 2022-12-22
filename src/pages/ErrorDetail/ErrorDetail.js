3181218f46f5d7b4cf28156776253912d6652e45
hkh0105 <hkh9601@naver.com>
2022-12-22 21:56:11 +0900

    ErrorDetail 페이지 리팩토링

--- src/pages/ErrorDetail/ErrorDetail.js
+++ src/pages/ErrorDetail/ErrorDetail.js
@@ -2,7 +2,7 @@ import "./ErrorDetail.scss";
 import Loadable from "../../components/Loadable/Loadable";
 import ErrorDetailsContainer from "../../components/ErrorDetailsContainer/ErrorDetaulsContainer";
 import ErrorHeader from "../../components/ErrorHeader/ErrorHeader";
-import BreadCrumbleContainerTemplate from "../../components/BreadCrumbleContainerTemplate/BreadCrumbleContainerTemplate";
+import BreadCrumbleContainer from "../../components/BreadCrumbleContainer/BreadCrumbleContainer";
 
 import useModeSellect from "../../hooks/useModeSellect";
 import useErrorDetail from "./useErrorDetail";
@@ -10,6 +10,15 @@ import useErrorDetail from "./useErrorDetail";
 export default function ErrorDetail() {
   const { selectedError } = useErrorDetail();
   const { mode, setMode } = useModeSellect("detail");
+  const {
+    stack,
+    breadcrumbsClick,
+    breadcrumbsURL,
+    message,
+    source,
+    createdAt,
+    user,
+  } = selectedError;
 
   const LoadableProps = {
     isLoading: !selectedError,
@@ -22,12 +31,21 @@ export default function ErrorDetail() {
   };
 
   const ErrorDetailsContainerProps = {
-    error: selectedError,
+    message,
+    source,
+    createdAt,
+    user,
+  };
+
+  const BreadCrumbleContainerProps = {
+    stack,
+    breadcrumbsClick,
+    breadcrumbsURL,
   };
 
   const ModeContainer = {
     detail: <ErrorDetailsContainer {...ErrorDetailsContainerProps} />,
-    breadcrumbs: <BreadCrumbleContainerTemplate />,
+    breadcrumbs: <BreadCrumbleContainer {...BreadCrumbleContainerProps} />,
   };
 
   return (
