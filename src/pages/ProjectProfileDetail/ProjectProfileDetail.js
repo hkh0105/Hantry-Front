eecc81da159325866e59daecc6634e506e0db032
hkh0105 <hkh9601@naver.com>
2022-12-23 21:34:33 +0900

    ProjectProfileDetail페이지 리팩토링

--- src/pages/ProjectProfileDetail/ProjectProfileDetail.js
+++ src/pages/ProjectProfileDetail/ProjectProfileDetail.js
@@ -1,25 +1,22 @@
 import { useParams, useLocation } from "react-router-dom";
-import ProfileDetailCard from "../../components/ProfileDetailCard/ProfileDetailCard";
-import Loading from "../../components/Loading/Loading";
+
 import useUserProject from "../../hooks/useUserProject";
-import PageHeader from "../../components/PageHeader/PageHeader";
 import Loadable from "../../components/Loadable/Loadable";
+import BulletBox from "../../components/BulletBox/BulletBox";
 
 export default function ProjectProfileDetail() {
-  const location = useLocation();
+  const { state: dsn } = useLocation();
   const type = useParams().type;
-  console.log(location, type);
-  const { dsn } = location.state;
-  const { profiles } = useUserProject(dsn);
+  const { profile } = useUserProject(dsn);
 
   return (
-    <Loadable isLoading={!profiles}>
-      <div className="profile-detail-container" style={{ marginTop: "5vh" }}>
+    <Loadable isLoading={!profile}>
+      <div className="profile-detail-container" style={{ marginTop: "2vh" }}>
         <h2>{type}</h2>
-        {Array.isArray(profiles[type]) &&
-          profiles[type].map((profile, i) => {
+        {Array.isArray(profile[type]) &&
+          profile[type].map((profile, i) => {
             if (profile) {
-              return <ProfileDetailCard profile={profile} key={i} />;
+              return <BulletBox profile={profile} key={i} />;
             }
           })}
       </div>
